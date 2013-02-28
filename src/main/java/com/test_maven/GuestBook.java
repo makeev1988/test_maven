a9ffd80a9aaf3df93318c5b4ed0f8cb937f468f4
makeev1988 <makeev.job@gmail.com>
2013-03-01 01:27:29 +0700

    исправил замечания

--- src/main/java/com/test_maven/GuestBook.java
+++ src/main/java/com/test_maven/GuestBook.java
@@ -20,30 +20,31 @@ public class GuestBook {
         try {
             controller = new ControllerRecords();
             String s;
-            while((s = in.readLine()) != null){
+            boolean exit = true;
+            while(exit &&((s = in.readLine()) != null)){
                 s = s.trim();
                 String [] st = s.split(" ");
                 String command = st[0].toUpperCase();
                 String message = s.substring(command.length(), s.length());
-                if (command.equals("ADD")){
-                    controller.addRecord(message);
-                } else if (command.equals("LIST")){
-                    System.out.println("id\tДата\t\t\t\t\t Сообщение:");
-                    List<Record> listRecords = controller.getRecords();
-                    for (Record r: listRecords){
-                        System.out.println(r.getRecordAsStream());
-                    }
-                } else if (command.equals("EXIT")){
-                    break;
-                } else {
-                    System.out.println("Неизвестная команда");
+                switch (command){
+                    case "ADD":
+                        controller.addRecord(message);
+                        break;
+                    case "LIST":
+                        System.out.println("id:\tДата:\t\t\t\t\t Сообщение:");
+                        List<Record> listRecords = controller.getRecords();
+                        for (Record r: listRecords){
+                            System.out.println(r.getRecordAsStream());
+                        }
+                        break;
+                    case "EXIT":
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("Неизвестная команда");
                 }
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (ClassNotFoundException | SQLException | IOException  e) {
             e.printStackTrace();
         } finally {
             try {
